f5f9d42cdcf57e5db346ab7b8047115a12e798d6
JeanPhilippeT <tourignyoffice@gmail.com>
2026-06-10 14:49:22 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         style={{
           width: "100%",
           height: "100vh",
-          objectFit: "contain",
+          objectFit: "cover",
         }}
       />
     </main>

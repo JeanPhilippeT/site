e76c682447029ee7f004ec6f89a212b5ff0cf494
JeanPhilippeT <tourignyoffice@gmail.com>
2026-06-11 11:54:54 +0100

    creat preview page

---
+++ src/app/preview/preview/page.tsx
@@ -0,0 +1,15 @@
+export default function Preview() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#f4ece6",
+        padding: "40px",
+      }}
+    >
+      <h1>Preview</h1>
+
+      <p>Version de travail du futur site JeanPhilippe Tourigny.</p>
+    </main>
+  );
+}
